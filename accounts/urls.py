183f28620fb3f429c79a2f8efcb61e06ca61a197
ALiHemati <alihemati502@gmail.com>
2023-10-20 14:33:18 +0330

    django project development

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register', views.RegisterView.as_view(), name='user_register' )
+    path('register', views.UserRegisterView.as_view(), name='user_register'),
+    path('login', views.UserLoginView.as_view(), name='user_login'),
+    path('logout', views.UserLogoutView.as_view(), name='user_logout'),
+    path('profile/<int:user_id>', views.UserProfileView.as_view(), name='user_profile'),
 ]
